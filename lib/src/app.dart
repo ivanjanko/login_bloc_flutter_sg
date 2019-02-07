8aad4690bf0f7f4fbe06b97573d868d7a1e450c9
ivan Jankovic <jankoemail@gmail.com>
2019-02-07 10:14:50 -0500

    scoped instance implementation

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 import 'screens/login_screen.dart';
+import 'blocs/provider.dart';
 
 class MyApp extends StatelessWidget {
   build(context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Log Me In',
-      home: Scaffold(
-        body: LoginScreen(),
-      )
+    return Provider(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Log Me In',
+        home: Scaffold(
+          body: LoginScreen(),
+        ),
+      ),
     );
   }
 }
